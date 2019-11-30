94d537289c3614e0109102e3f865dd06ec5ca809
tuktuk4944 <padrada.ttpp@gmail.com>
2019-11-30 16:44:43 +0700

    new and save product

--- saveproduct.php
+++ saveproduct.php
@@ -1,15 +1,20 @@
 <?php 
-    $allowedType=["jpg","jpg","gif","png","tif","tiff"];
+    $allowedType=["jpg","jpeg","gif","png","tif","tiff"];
     $fileType=explode("/",$_FILES["filepic"]["type"]);
     //image/png filetype=["image","png"]
     if(!in_array($fileType[1],$allowedType)){
         //เมื่ออัปโหลดไฟล์ที่ไม่ตรงกับ type ใน aloowType
         echo "Non-Image file is not allowed. <br>";
     }
-    echo "Type: ". $_FILES["filepic"]["type"]."<br>";
-    echo "Name: ". $_FILES["filepic"]["name"]."<br>";
-    echo "Size: ". $_FILES["filepic"]["size"]."<br>";
-    echo "Tmp Name: ". $_FILES["filepic"]["tmp_name"]."<br>";
-    echo "Error: ". $_FILES["filepic"]["error"]."<br>";
+    else{
+        echo "Type: ". $_FILES["filepic"]["type"]."<br>";
+        echo "Name: ". $_FILES["filepic"]["name"]."<br>";
+        echo "Size: ". $_FILES["filepic"]["size"]."<br>";
+        echo "Tmp Name: ". $_FILES["filepic"]["tmp_name"]."<br>";
+        echo "Error: ". $_FILES["filepic"]["error"]."<br>";
+
+        move_uploaded_file($_FILES["filepic"]["tmp_name"],"img/product/".$_FILES["filepic"]["name"]);
+    }
+    
 
 ?>
